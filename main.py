8a03c13c692cf0b3523f7c54d22168579ada783a
Emily Rivera <ekr010@bucknell.edu>
2024-11-19 16:45:24 -0500

    main file created

---
+++ main.py
@@ -0,0 +1,7 @@
+'''
+City Connections Project Due 12/06/2024
+
+Project by: Bryce Babcock, Tj Freeman, and Emily Rivera
+
+Sources: Help from Copilot, specific sources are listed with each function
+'''
