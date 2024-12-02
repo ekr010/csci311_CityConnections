cedfdf4901bafaa5429ac39687d566f4fa205374
TJ  Freeman <tf015@bucknell.edu>
2024-12-02 12:24:52 -0500

    Started Kruskal

---
+++ Algorithms/DFS.py
@@ -0,0 +1,10 @@
+
+def DFSWrapper(V,E):
+    visited = []
+
+    for u in V:
+        # mark u as visited
+        visited.append(u)
+
+def DFSRecurse(s,time):
+    
---
+++ Algorithms/kruskal.py
@@ -0,0 +1,25 @@
+'''
+Kruskal Algorithm for Minimum Spanning Tree
+
+TJ Freeman
+'''
+
+from quicksort import *
+from DFS import *
+
+def KruskalMST(V,E,w):
+    # Sort E in order of increasing weight
+    quickSort(E)
+
+    T = []
+
+    for e in sortedE:
+        # Check if there's a cycle
+        for ei in E 
+            if ei 
+        if 
+            # add e to T
+            T.append(e) 
+
+    return T
+
